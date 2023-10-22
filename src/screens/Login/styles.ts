0385b032f5441c1fde56de8c433e8c12b15fb27f
Deny Barbosa <deny.stanley.06@gmail.com>
2023-10-22 20:02:05 -0300

    feat: add login screen

---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,9 @@
+import { styled} from '@mui/material/styles';
+import { CardHeader } from '@mui/material';
+
+export const CustomizedCardHeader = styled(CardHeader)`
+    .MuiCardHeader-title {
+        font-size: 96px;
+        
+    }
+`;
