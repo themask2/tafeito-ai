68fb14a731caa366c0cba4604d8a84efd63b8d90
Deny Barbosa <deny.stanley.06@gmail.com>
2023-10-28 22:10:40 -0300

    feat: Adding main area, reading categories from DB and inserting new tasks on DB.

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,71 @@
+import { Box, Typography } from "@mui/material";
+
+import { CustomizedSectionBox } from "./styles";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { url_categorias } from "../../utils/api";
+import { Categoria } from "../../utils/model";
+
+import { MainProps } from "./Main";
+import TaskInput from "../TaskInput";
+
+const Main = (props: MainProps) => {
+  const { categorias } = props;
+
+  const [selectedTaskInput, setSelectedTaskInput] = useState<string | null>(
+    null
+  );
+
+  const renderCategoriaSection = (categoria_item: Categoria) => {
+    return (
+      <CustomizedSectionBox key={categoria_item.id}>
+        <Typography variant="h2"> {categoria_item.descricao} </Typography>
+        <div>TODO: Listar Tarefas {categoria_item.descricao}</div>
+        {selectedTaskInput === null ||
+        selectedTaskInput === categoria_item.descricao ? (
+          <TaskInput
+            category={categoria_item}
+            onSelectCreateTask={(category) => {
+              setSelectedTaskInput(category);
+            }}
+          />
+        ) : null}
+      </CustomizedSectionBox>
+    );
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexWrap={"wrap"}
+      sx={{
+        margin: "8px 16px 24px 16px",
+      }}
+    >
+      <CustomizedSectionBox>
+        <Typography variant="h1"> Suas tarefas </Typography>
+      </CustomizedSectionBox>
+      {categorias.map((categoria) => renderCategoriaSection(categoria))}
+    </Box>
+  );
+};
+
+const MainWrapper = () => {
+  const [categorias, setCategorias] = useState<null | Categoria[]>(null);
+
+  useEffect(() => {
+    axios.get(url_categorias).then((response) => {
+      console.log("xxx", response.data);
+      setCategorias(response.data);
+    });
+  }, []);
+
+  if (categorias !== null) {
+    return <Main categorias={categorias} />;
+  }
+
+  return <div>Loading!!</div>;
+};
+
+export default MainWrapper;
--- src/screens/Tasks/index.tsx
+++ src/screens/Tasks/index.tsx
@@ -1,20 +1,23 @@
 import * as React from "react";
-import NavBar from '../../components/NavBar';
-import { useNavigate } from 'react-router-dom';
+import NavBar from "../../components/NavBar";
+import { useNavigate } from "react-router-dom";
+import Main from "../../components/Main";
 
-import { useAuth } from '../../provider/authProvider';
+import { useAuth } from "../../provider/authProvider";
 const Tasks = () => {
-
   const navigate = useNavigate();
   const { setToken } = useAuth();
 
   const logout = () => {
     setToken(null);
     navigate("/login", { replace: true });
-  }
+  };
 
   return (
-    <NavBar logout={logout} />
+    <>
+      <NavBar logout={logout} />
+      <Main />
+    </>
   );
 };
 
